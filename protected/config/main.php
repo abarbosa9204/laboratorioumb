9e7e17c9cd97df6217fc281dc09388e3f2bb52fb
Angel Barbosa <abarbosa@equitel.com.co>
2022-04-28 16:36:57 -0500

    Loading Bootstrap 5 template

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,8 +7,8 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
-
+	'name'=>'Laboratorio UMB',
+	'defaultController' => 'site/login',
 	// preloading 'log' component
 	'preload'=>array('log'),
 
@@ -39,16 +39,17 @@ return array(
 		),
 
 		// uncomment the following to enable URLs in path-format
-		/*
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+		'urlManager' => array(
+			'urlFormat' 		=> 'path',
+			'showScriptName' 	=> false,
+			'urlSuffix'			=> '.asp',
+			'rules' 			=> array(
+				//'<action:(index)>'=>'site/<action>',
+				'<controller:\w+>/<id:\d+>' 				=> '<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>' 	=> '<controller>/<action>',
+				'<controller:\w+>/<action:\w+>' 			=> '<controller>/<action>',
 			),
 		),
-		*/
 
 		// database settings are configured in database.php
 		'db'=>require(dirname(__FILE__).'/database.php'),
