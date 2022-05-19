0ef1b0a51f821111ce8146ef71ef02bfc3525752
opeinado <opeinado@gmail.com>
2022-05-18 21:03:00 -0500

    loading form source code

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -73,7 +73,7 @@
 
         <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
           <div class="app-brand demo">
-            <a href="index.html" class="app-brand-link">
+            <a href="<?= Yii::app()->baseUrl; ?>/site/index" class="app-brand-link">
               <span class="app-brand-logo demo">
 					<img src="<?php echo Yii::app()->baseUrl; ?>/dist/assets/img/illustrations/icon.png" alt="UMB" width="40">
               </span>
